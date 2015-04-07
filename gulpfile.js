ef8fbd343ad6d437c336309ea75a23fc8abb8fe6
vzvu3k6k <vzvu3k6k@gmail.com>
2015-04-08 06:12:00 +0900

    Introduce watchify to gulpfile.js
    https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var reactify = require('reactify');
+var assign = require('lodash.assign');
+
+var bOpts = assign({}, watchify.args, {
+  entries: './js/webpage_main.js',
+  transform: [reactify]
+});
+var b = watchify(browserify(bOpts));
 
 gulp.task('default', ['build']);
 
 gulp.task('build', ['build:js', 'build:html', 'build:css']);
 
-gulp.task('build:js', function() {
-  return browserify({
-    entries: './js/webpage_main.js',
-    transform: [reactify]
-  }).bundle()
-    .pipe(source('index.js'))
-    .pipe(gulp.dest('dist/js'));
-});
+gulp.task('build:js', bundle);
+b.on('update', bundle);
+b.on('log', gutil.log);
 
 gulp.task('build:html', function() {
   return gulp.src('html/*.html').pipe(gulp.dest('dist'));
@@ -23,3 +27,10 @@ gulp.task('build:html', function() {
 gulp.task('build:css', function() {
   return gulp.src('css/*.css').pipe(gulp.dest('dist/css'));
 });
+
+function bundle(){
+  return b.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('dist/js'));
+}
